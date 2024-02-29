e960a556892face337ef2a2769d4c13fbe898660
Curt D Francis <139297143+Curt-D-Francis@users.noreply.github.com>
2024-02-28 20:15:10 -0500

    Update test.session.sql

--- test.session.sql
+++ test.session.sql
@@ -1,14 +1,4 @@
---@block
-CREATE TABLE UserInput(
-    id INT PRIMARY KEY,
-    actualURL VARCHAR(255)
-);
 
---@block
-CREATE TABLE OutputURL(
-    id INT PRIMARY KEY,
-    newURL VARCHAR(255)
-)
 --@block
 CREATE TABLE URLMappings (
     id INT PRIMARY KEY AUTO_INCREMENT,
